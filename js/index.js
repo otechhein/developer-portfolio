ad19dcd4882fca73757b359519201fc14ee46959
otechhein <otechhein27923@gmail.com>
2023-10-31 16:50:13 +0630

    project portfolio carousel setup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+/* Smooth Effect for Links */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
